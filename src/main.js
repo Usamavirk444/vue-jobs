f712b4b117f45212f4a1d01a0ebd83dbb68e889f
Usama <virkusama3@gmail.com>
2025-03-09 03:56:34 +0500

    initial commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './assets/main.css'
+import router from './router'
+import 'primeicons/primeicons.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+const app =  createApp(App);
+app.use(router)
+app.mount('#app')
